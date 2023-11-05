788bd744a1a2650faafbc42bb561e0cdb0d4820a
Divya <singhdivya10aug2002@gmail.com>
2023-11-05 19:08:32 +0530

    Mdn Todo completed

--- 2.Todo/src/components/FilterButton.js
+++ 2.Todo/src/components/FilterButton.js
@@ -1,13 +1,18 @@
-import React from 'react'
+import React from "react";
 
 function FilterButton(props) {
   return (
-    <button type='button' className='btn toggle-btn' aria-pressed='true'>
-      <span className='visually-hidden'>Show </span>
-      <span>all </span>
-      <span className='visually-hidden'> tasks</span>
+    <button
+      type="button"
+      className="btn toggle-btn"
+      aria-pressed={props.isPressed}
+      onClick={() => props.setFilter(props.name)}
+    >
+      <span className="visually-hidden">Show </span>
+      <span>{props.name}</span>
+      <span className="visually-hidden"> tasks</span>
     </button>
-  )
+  );
 }
 
-export default FilterButton
+export default FilterButton;
