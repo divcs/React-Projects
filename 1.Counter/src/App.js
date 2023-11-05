7f488158c005da1973d35ba8599cad3c06abea14
Divya <singhdivya10aug2002@gmail.com>
2023-11-05 14:43:37 +0530

    Counter functionality completed

--- 1.Counter/src/App.js
+++ 1.Counter/src/App.js
@@ -1,5 +1,26 @@
+import { useState } from 'react'
 function App() {
-  return <></>
+  const [value, setValue] = useState(0)
+
+  function increment(value) {
+    setValue(value + 1)
+  }
+
+  function decrement(value) {
+    setValue(value - 1)
+  }
+  function reset(value) {
+    setValue(0)
+  }
+
+  return (
+    <>
+      <h1>Counter: {value}</h1>
+      <button onClick={() => increment(value)}>Increment</button>
+      <button onClick={() => reset(value)}>Reset</button>
+      <button onClick={() => decrement(value)}>Decrement</button>
+    </>
+  )
 }
 
 export default App
