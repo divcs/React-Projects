6af1e511c839314c0ca6f52913e0a1abe1979d21
Divya <singhdivya10aug2002@gmail.com>
2023-11-06 19:44:04 +0530

    6.Hidden-Search-Bar

---
+++ 6.Search-Icon-Project/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg'
+import './App.css'
+import HiddenSeachBar from './HiddenSeachBar'
+
+function App() {
+  return (
+    <>
+      <HiddenSeachBar />
+    </>
+  )
+}
+
+export default App
---
+++ 6.Search-Icon-Project/src/HiddenSeachBar.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+import { FaSearch } from 'react-icons/fa'
+
+const HiddenSeachBar = () => {
+  const [showInput, setShowInput] = useState(false)
+  const [bgColor, setBgColor] = useState('white')
+
+  const handleClick = (e) => {
+    setBgColor('#1a1a1a')
+    if (e.target.className === 'container') {
+      setShowInput(false)
+      setBgColor('#fff')
+    }
+  }
+
+  return (
+    <section
+      className='container'
+      style={{ backgroundColor: bgColor }}
+      onClick={handleClick}
+    >
+      {showInput ? (
+        <input type='text' placeholder='Search...' />
+      ) : (
+        <FaSearch onClick={() => setShowInput(true)} />
+      )}
+    </section>
+  )
+}
+
+export default HiddenSeachBar
