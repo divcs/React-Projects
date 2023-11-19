c0ae81ea57b574b3d0673b8ad1d008b3984ad23a
Divya <singhdivya10aug2002@gmail.com>
2023-11-19 17:17:32 +0530

    Updating Stripe submenus

--- 22. Stripe-submenus/src/App.js
+++ 22. Stripe-submenus/src/App.js
@@ -1,5 +1,8 @@
 import React from 'react'
-
+import Navbar from './Navbar'
+import Hero from './Hero'
+import Sidebar from './Sidebar'
+import Submenu from './Submenu'
 function App() {
   return (
     <>
