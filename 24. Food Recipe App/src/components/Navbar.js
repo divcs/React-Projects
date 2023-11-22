31125bda8d7668a1aa776fa64b06c2fbbde557da
Divya <singhdivya10aug2002@gmail.com>
2023-11-22 16:37:30 +0530

    Styling Navbar

--- 24. Food Recipe App/src/components/Navbar.js
+++ 24. Food Recipe App/src/components/Navbar.js
@@ -1,7 +1,18 @@
 import React from 'react'
 
 const Navbar = () => {
-  return <div className='navbar'>Navbar</div>
+  return (
+    <div className='navbar container'>
+      <a href='#!' className='logo'>
+        F<span>oo</span>diesHub
+      </a>
+      <div className='nav-links'>
+        <a href='#!'>Home</a>
+        <a href='#!'>Recipes</a>
+        <a href='#!'>Settings</a>
+      </div>
+    </div>
+  )
 }
 
 export default Navbar
