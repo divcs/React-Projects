5f5aa4f180d0dbdcbf74359d8d41027952552e9f
Divya <singhdivya10aug2002@gmail.com>
2023-12-12 17:00:36 +0530

    Food Recipes

---
+++ 27. Food Recipe Web App/src/pages/Home.js
@@ -0,0 +1,15 @@
+import HeroSection from '../components/HeroSection'
+import ImproveSkills from '../components/ImproveSkills'
+import QuoteSection from '../components/QuoteSection'
+import ChiefsSection from '../components/ChiefsSection'
+
+export default function Home() {
+  return (
+    <div>
+      <HeroSection />
+      <ImproveSkills />
+      <QuoteSection />
+      <ChiefsSection />
+    </div>
+  )
+}
